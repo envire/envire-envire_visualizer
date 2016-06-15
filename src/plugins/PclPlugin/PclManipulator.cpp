af2ba7f204ad71383a288d0168cecb5eb84b5cfb
Arne Böckmann <arne.boeckmann@dfki.de>
2016-06-15 17:01:49 +0200

    pcl item manipulator

--- src/plugins/PclPlugin/PclManipulator.cpp
+++ src/plugins/PclPlugin/PclManipulator.cpp
@@ -2,6 +2,9 @@
 #include <QHBoxLayout>
 #include <QLabel> //FIXME remove after test
 #include <envire_pcl/PointCloud.hpp>
+#include <envire_core/graph/GraphTypes.hpp>
+#include <QTableWidget>
+#include <QHeaderView>
 
 using namespace envire::viz;
 using namespace envire::core;
@@ -18,7 +21,7 @@ QWidget* PclItemManipulatorFactory::create(ItemBase::Ptr selectedItem,
                                            const envire::core::TreeView& treeView)
 {
   //FIXME do type check
-  return new PclItemManipulator();
+  return new PclItemManipulator(selectedItem, graph, treeView);
 }
 
 const std::vector<std::type_index>& PclItemManipulatorFactory::getSupportedTypes()
@@ -26,11 +29,71 @@ const std::vector<std::type_index>& PclItemManipulatorFactory::getSupportedTypes
   return types;
 }
 
-
-
-PclItemManipulator::PclItemManipulator(QWidget* parent): QWidget(parent)
+PclItemManipulator::PclItemManipulator(ItemBase::Ptr selectedItem,
+                                       std::shared_ptr< EnvireGraph > graph,
+                                       const TreeView& treeView, QWidget* parent) :
+  QWidget(parent), selectedItem(selectedItem), graph(graph), treeView(treeView)
 {
-  QHBoxLayout* layout = new QHBoxLayout(this);
-  layout->addWidget(new QLabel("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"));
-  setLayout(layout);
+    QLabel* alignTo = new QLabel("Align to:", this);
+    QVBoxLayout* layout = new QVBoxLayout(this);
+    layout->addWidget(alignTo);
+    
+    
+    itemTable = new QTableWidget(this);
+    layout->addWidget(itemTable);
+    setLayout(layout);
+    
+    
+    connect(itemTable, SIGNAL(itemSelectionChanged()), this, SLOT(itemSelectionChanged()));
+    
+    std::vector<ItemBase::Ptr> otherItems = findMatchingItems(selectedItem->getTypeIndex());
+    itemTable->setRowCount(otherItems.size());
+    itemTable->setColumnCount(2);
+    for(int i = 0; i < otherItems.size(); ++i)
+    {
+      const ItemBase::Ptr& item = otherItems[i];
+      itemTable->setItem(i, 0, new QTableWidgetItem(QString::fromStdString(item->getFrame())));
+      itemTable->setItem(i, 1, new QTableWidgetItem(QString::fromStdString(item->getIDString())));
+    }
+    QStringList header;
+    header << "Frame" << "Item";
+    itemTable->setHorizontalHeaderLabels(header);
+    itemTable->setSelectionBehavior(QAbstractItemView::SelectRows);
+    itemTable->setSelectionMode(QAbstractItemView::SingleSelection);
+    itemTable->verticalHeader()->setVisible(false);
+    if(otherItems.size() > 0)
+      itemTable->selectRow(0);
+
+    
 }
+
+std::vector<ItemBase::Ptr> PclItemManipulator::findMatchingItems(const std::type_index& type) const
+{
+  std::vector<ItemBase::Ptr> items;
+  using Vd = GraphTraits::vertex_descriptor;
+  treeView.visitDfs(treeView.root, [this, &items, &type] (Vd node, Vd parent)
+  {
+    if(this->graph->containsItems(node, type))
+    {
+      const envire::core::Frame::ItemList& itemsInFrame = this->graph->getItems(node, type);
+      items.insert(items.end(), itemsInFrame.begin(), itemsInFrame.end());
+    }
+  });
+  return items;
+}
+
+void PclItemManipulator::itemSelectionChanged()
+{
+  QItemSelectionModel* select = itemTable->selectionModel();
+  if(select->hasSelection())
+  {
+    QModelIndexList rows = select->selectedRows();
+    assert(rows.size() > 0);
+    //multiselect is disbled, thus all elements in the list belong to the same row
+    const int row = rows.at(0).row();
+    std::cout << "ROW: " << row << std::endl;
+  }
+}
+
+
+
--- src/plugins/PclPlugin/PclManipulator.hpp
+++ src/plugins/PclPlugin/PclManipulator.hpp
@@ -2,6 +2,8 @@
 #include "envire_visualizer/plugins/ItemManipulatorFactoryInterface.hpp"
 #include <QWidget>
 
+class QTableWidget;
+
 namespace envire { namespace viz 
 {
 class PclItemManipulatorFactory : public ItemManipulatorFactoryInterface
@@ -24,7 +26,23 @@ class PclItemManipulator : public QWidget
 {
   Q_OBJECT
 public:
-  PclItemManipulator(QWidget* parent = nullptr);
+  PclItemManipulator(core::ItemBase::Ptr selectedItem,
+                          std::shared_ptr<envire::core::EnvireGraph> graph,
+                          const core::TreeView& treeView, QWidget* parent = nullptr);
+  
+private slots:
+  //called when the user selects a different item for icp alignment
+  void itemSelectionChanged();
+private:
+  
+  /**Returns a list of all items of @p type in the graph */
+  std::vector<envire::core::ItemBase::Ptr> findMatchingItems(const std::type_index& type) const;
+  
+  envire::core::ItemBase::Ptr selectedItem;
+  envire::core::ItemBase::Ptr alignToItem;
+  std::shared_ptr<envire::core::EnvireGraph> graph;
+  const core::TreeView& treeView;
+  QTableWidget* itemTable;
 };
 
 
